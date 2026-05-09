9be7856539f21c07968079ba488650da05a910aa
yogeshkukadiya92-stack <yogeshkukadiya92@gmail.com>
2026-05-09 12:09:16 +0530

    Route app root to workshop scheduling admin page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { BusinessOS } from "@/components/business-os";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  return <BusinessOS />;
+  redirect("/workshop-scheduling-admin");
 }
